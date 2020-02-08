7f88f58af8f4dc278a8591a1d392bfadb6f002eb
Louis <louis.jolliet@etu.u-bordeaux.fr>
2020-02-08 14:54:46 +0100

    user, playlist and track generator

--- src/MixBay/app/src/main/java/fr/pdp/mixbay/models/Session.java
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/models/Session.java
@@ -2,9 +2,9 @@ package fr.pdp.mixbay.models;
 
 import android.content.Context;
 
+import java.util.HashSet;
 import java.util.Set;
 
-import fr.pdp.mixbay.MainActivity;
 
 public class Session {
 
@@ -15,17 +15,41 @@ public class Session {
     private AlgoI algo;
     private LogManagerI logManager;
     private APIManagerI apiManager;
-    
-    public Session(){
-        
-    }
 
     public Session(APIManagerI api) {
         this.apiManager = api;
+        this.localePlaylist = new Playlist("01", "MixBay Playlist");
+        this.users = new HashSet<>();
     }
 
-    public void start(Context mainActivity) {
-        apiManager.connect(mainActivity);
+    public void start(Context context) {
+        apiManager.connect(context);
+        randomInit();
+    }
+
+    public void mix(){
+        localePlaylist = algo.compute();
+    }
+
+    @Deprecated
+    private void randomInit(){
+        mainUser = new User("123", "tmp");
+        User secondUser = new User("456","tmp2");
+        currentuser = mainUser;
+        users.add(mainUser);
+        users.add(secondUser);
+        int playlistID = 0;
+        int trackID = 0;
+
+        for(User u : users)
+        {
+            Playlist p = new Playlist(Integer.toString(playlistID), "Playlist "+ playlistID);
+            for(int i=0; i< 100; i++){
+                p.addTrack(new Track(Integer.toString(trackID), "Track " + trackID, "Random Album", "Random Artist", "Random Cover"));
+            }
+            u.addPlaylist(p);
+        }
+        System.out.println(users);
     }
 
     public void end() {
