d9d69336bd4cb3090b2cd1c741106bc88e67de2f
Paul Vigneau <paul.vigneau@etu.u-bordeaux.fr>
2020-02-08 19:42:20 +0100

    Change authentication method

--- src/MixBay/app/src/main/java/fr/pdp/mixbay/models/APIManagerI.java
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/models/APIManagerI.java
@@ -6,11 +6,14 @@ import java.util.Set;
 
 public interface APIManagerI {
 
+    int SPOTIFY_REQUEST_CODE = 1337;
+
     public boolean connect(Context context);
     public boolean disconnect();
     public User getUser(String id);
     public Set<Playlist> getUserPlaylist(String id);
     public void playCurrentTrack();
     public void playTrack(String id);
+    public void setAccessToken(String token);
 
 }
--- src/MixBay/app/src/main/java/fr/pdp/mixbay/models/SpotifyAPI.java
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/models/SpotifyAPI.java
@@ -1,16 +1,26 @@
 package fr.pdp.mixbay.models;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 
-import com.spotify.android.appremote.api.ConnectionParams;
-import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.Set;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class SpotifyAPI implements APIManagerI {
@@ -18,11 +28,19 @@ public class SpotifyAPI implements APIManagerI {
 
     private static final String CLIENT_ID = "cb64858e27964a13b85d3e7e5e19b1d3";
     private static final String REDIRECT_URI = "mixbay://callback";
+    private static final String[] SCOPES = new String[]{"app-remote-control", "user-read-email", "user-read-private"};
+
     private SpotifyAppRemote mSpotifyAppRemote;
+    private Context context;
+
+    private final OkHttpClient requestClient = new OkHttpClient();
+    private String accessToken;
 
     @Override
     public boolean connect(Context context) {
-        ConnectionParams connectionParams =
+        this.context = context;
+
+        /*ConnectionParams connectionParams =
                 new ConnectionParams.Builder(CLIENT_ID)
                         .setRedirectUri(REDIRECT_URI)
                         .showAuthView(true)
@@ -41,14 +59,29 @@ public class SpotifyAPI implements APIManagerI {
                         Log.e("MainActivity", throwable.getMessage(), throwable);
                     }
                 });
-        return mSpotifyAppRemote!= null && mSpotifyAppRemote.isConnected();
+        return mSpotifyAppRemote!= null && mSpotifyAppRemote.isConnected();*/
+
+        AuthorizationRequest.Builder builder =
+                new AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(SCOPES);
+        AuthorizationRequest request = builder.build();
+
+        AuthorizationClient.openLoginActivity((Activity) context, SPOTIFY_REQUEST_CODE, request);
+
+        return true;
     }
 
 
     @Override
     public boolean disconnect() {
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-        return false;
+        //SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+
+        if (context == null)
+            return false;
+
+        AuthorizationClient.clearCookies(context);
+        return true;
     }
 
     @Override
@@ -81,5 +114,9 @@ public class SpotifyAPI implements APIManagerI {
                         Log.d("MainActivity", track.name + " by " + track.artist.name);
                     }
                 });
+    }
+
+    public void setAccessToken(String token) {
+        this.accessToken = token;
     }
 }
