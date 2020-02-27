cab123e3fbf434e5388020719ab0c6df435be55e
Louis <louis.jolliet@etu.u-bordeaux.fr>
2020-02-27 16:50:08 +0100

    Adding PlayerException

---
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/business/exceptions/PlayerException.java
@@ -0,0 +1,10 @@
+package fr.pdp.mixbay.business.exceptions;
+
+public class PlayerException extends RuntimeException {
+
+    public PlayerException(String message) {
+        super(message);
+        //TODO manage exception
+    }
+
+}
