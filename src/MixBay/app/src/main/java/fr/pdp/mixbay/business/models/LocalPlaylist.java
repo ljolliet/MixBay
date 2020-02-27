e9de7a76ec7b5ab05b00a519ef26855fe5e83542
Louis <louis.jolliet@etu.u-bordeaux.fr>
2020-02-27 16:49:34 +0100

    Current track management

--- src/MixBay/app/src/main/java/fr/pdp/mixbay/business/models/LocalPlaylist.java
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/business/models/LocalPlaylist.java
@@ -2,19 +2,29 @@ package fr.pdp.mixbay.business.models;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import fr.pdp.mixbay.business.exceptions.PlayerException;
 
 public class LocalPlaylist implements PlaylistI {
 
     public static final int SIZE_MAX = 25;
-    private int currentTrack = 0;
+    private Track currentTrack;
     private List<Track> tracks = new ArrayList<>();
 
 
     public Track getCurrentTrack() {
-        return this.tracks.get(currentTrack);
+        return currentTrack;
+    }
+
+    public void setCurrentTrack(String id){
+        this.currentTrack = null;
+        for(Track t : this.tracks)
+            if(id.equals(t.id))
+                this.currentTrack = t;
+        if (this.currentTrack == null)
+            throw new PlayerException("Current track not found in Local Playlist");
     }
 
     @Override
@@ -29,6 +39,6 @@ public class LocalPlaylist implements PlaylistI {
 
     @Override
     public Collection<Track> getTracks() {
-        return new HashSet<>(this.tracks);
+        return new ArrayList<>(this.tracks);
     }
 }
