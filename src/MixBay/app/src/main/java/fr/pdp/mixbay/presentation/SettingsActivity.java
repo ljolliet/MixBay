001cc94b5b5a259e1bf778c0ad6eaaac399ddd72
Paul Vigneau <paul.vigneau@etu.u-bordeaux.fr>
2020-02-28 17:25:41 +0100

    Add Settings Activity

---
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/presentation/SettingsActivity.java
@@ -0,0 +1,16 @@
+package fr.pdp.mixbay.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.pdp.mixbay.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
