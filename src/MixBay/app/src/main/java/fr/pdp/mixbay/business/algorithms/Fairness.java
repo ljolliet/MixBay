f7f53ba99a238f0dad60d0902d64dc0bcb510475
Nicolas Deguillaume <nicolas.deguillaume@emi.u-bordeaux.fr>
2020-03-07 20:40:26 +0100

    Most pleasure & Fairness strategies

---
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/business/algorithms/Fairness.java
@@ -0,0 +1,39 @@
+package fr.pdp.mixbay.business.algorithms;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import fr.pdp.mixbay.business.models.LocalPlaylist;
+import fr.pdp.mixbay.business.models.Track;
+import fr.pdp.mixbay.business.models.User;
+
+public class Fairness extends AlgoSuperclass{
+
+    @Override
+    public String getName() {
+        return this.getClass().toString() + " with " + score.getClass().toString();
+    }
+
+    @Override
+    public LocalPlaylist computeLocalePlaylist(Map<String, Map<String, Double>> musicScorePerUser, Set<User> users, Map<String, Track> tracksList) {
+        LocalPlaylist p = new LocalPlaylist();
+        int index = 0;
+        boolean empty = false;
+        while(index < LocalPlaylist.SIZE_MAX && !empty) {
+            Iterator usersIt = users.iterator();
+            while (usersIt.hasNext() && index++ < LocalPlaylist.SIZE_MAX && !empty) {
+                User currentUser = (User) usersIt.next();
+                Iterator tracksIt = musicScorePerUser.get(currentUser.id).entrySet().iterator();
+                Map.Entry trackEntry = (Map.Entry) tracksIt.next();
+                Track t = tracksList.get(trackEntry.getKey());
+                p.addTrack(t);
+                for (User u : users) {
+                    musicScorePerUser.get(u.id).remove(trackEntry.getKey());
+                }
+                empty = musicScorePerUser.get(currentUser.id).size() == 0;
+            }
+        }
+        return p;
+    }
+}
---
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/business/algorithms/MostPleasure.java
@@ -0,0 +1,54 @@
+package fr.pdp.mixbay.business.algorithms;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import fr.pdp.mixbay.business.models.LocalPlaylist;
+import fr.pdp.mixbay.business.models.Track;
+import fr.pdp.mixbay.business.models.User;
+import fr.pdp.mixbay.business.utils.MapUtil;
+
+public class MostPleasure extends AlgoSuperclass {
+
+    @Override
+    public String getName() {
+        return this.getClass().toString() + " with " + score.getClass().toString();
+    }
+
+    /**
+     * Compute a local playlist of at most Playlist.SIZE_MAX (25) tracks
+     * The music score of a track for the group is the highest score of all the users (Most Pleasure Strategy).
+     * The 25 tracks with the lowest distance are chosen to be part of the locale playlist.
+     *
+     * @param musicScorePerUser A structure containing the computed personnal score of each track for each user
+     * @param users A set of users with datas loaded from a streaming API
+     * @param tracksList A set of all the tracks extracted from all the playlists
+     * @return A playlist of at most 25 tracks
+     */
+    public LocalPlaylist computeLocalePlaylist(Map<String, Map<String, Double>> musicScorePerUser, Set<User> users, Map<String, Track> tracksList) {
+        Map<String, Double> MostPleasureComputed = new LinkedHashMap<>();
+        Iterator tracksIt = tracksList.entrySet().iterator();
+        while (tracksIt.hasNext()) {
+            Map.Entry trackEntry = (Map.Entry) tracksIt.next();
+            Track t = (Track) trackEntry.getValue();
+            Double maxScore = 100.;
+            for (User u : users) {
+                if ((musicScorePerUser.get(u.id)).get(t.id) < maxScore) {
+                    maxScore = (musicScorePerUser.get(u.id)).get(t.id);
+                }
+            }
+            MostPleasureComputed.put(t.id, maxScore);
+        }
+        MostPleasureComputed = MapUtil.sortByValue(MostPleasureComputed);
+        Iterator computedTracksIterator = MostPleasureComputed.entrySet().iterator();
+        int index = 0;
+        LocalPlaylist p = new LocalPlaylist();
+        while (computedTracksIterator.hasNext() && index < LocalPlaylist.SIZE_MAX) {
+            Map.Entry entryTrack = (Map.Entry) computedTracksIterator.next();
+            p.addTrack(tracksList.get(entryTrack.getKey()));
+            index++;
+        }
+        return p;
+    }
+}
