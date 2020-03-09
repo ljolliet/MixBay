acd721337928086f2ed54aaf74c1b67303b8ff6a
Paul Vigneau <paul.vigneau@etu.u-bordeaux.fr>
2020-03-10 00:30:16 +0100

    Add real users beside track in the playlist display

--- src/MixBay/app/src/main/java/fr/pdp/mixbay/business/utils/TrackAdapter.java
+++ src/MixBay/app/src/main/java/fr/pdp/mixbay/business/utils/TrackAdapter.java
@@ -1,30 +1,33 @@
 package fr.pdp.mixbay.business.utils;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import fr.pdp.mixbay.R;
 import fr.pdp.mixbay.business.models.Track;
-
-//TODO comments
+import fr.pdp.mixbay.business.models.User;
 
 public class TrackAdapter extends BaseAdapter {
 
     private List<Track> trackList;
     private LayoutInflater inflater;
+    private Context context;
 
     public TrackAdapter(Context context, List<Track> trackList) {
         this.trackList = trackList;
         this.inflater = LayoutInflater.from(context);
+        this.context = context;
     }
 
     @Override
@@ -48,20 +51,41 @@ public class TrackAdapter extends BaseAdapter {
         if(convertView == null)
             convertView = inflater.inflate(R.layout.list_track_item, parent, false);
 
+        Track track = (Track) getItem(position);
+        List<User> userList = new ArrayList<>(Factory.getUsersForTrack(track));
+
         TrackViewHolder viewHolder = (TrackViewHolder) convertView.getTag();
-        if(viewHolder == null){
+        if(viewHolder == null) {
             viewHolder = new TrackViewHolder();
             viewHolder.title = convertView.findViewById(R.id.trackTitle);
             viewHolder.artist = convertView.findViewById(R.id.artistName);
-            viewHolder.userInitial = convertView.findViewById(R.id.userInitial);
+            viewHolder.initialTextView = convertView.findViewById(R.id.userInitial);
+            viewHolder.otherUsersTextView = convertView.findViewById(R.id.otherUsers);
+
             convertView.setTag(viewHolder);
         }
 
-        Track track = (Track) getItem(position);
-
         viewHolder.title.setText(track.getTitle());
         viewHolder.artist.setText(makeArtistsString(track));
-//        viewHolder.userInitial.setText(track.getUser().getInitial());
+        // Set initial of 1 user
+        viewHolder.initialTextView.setText(String.valueOf(userList.get(0).initial));
+        // Set color of the View
+        Drawable background = viewHolder.initialTextView.getBackground();
+        ((GradientDrawable) background).setColor(userList.get(0).getColor());
+
+        // Display TextView if they are other users
+        if (userList.size() > 1) {
+            viewHolder.otherUsersTextView.setVisibility(View.VISIBLE);
+            String size = "+" + (userList.size() - 1);
+            viewHolder.otherUsersTextView.setText(size);
+
+            // Set color of the View
+            Drawable background1 = viewHolder.otherUsersTextView.getBackground();
+            ((GradientDrawable) background1).setColor(context.getResources().getColor(R.color.grey, context.getTheme()));
+        }
+        // Else, set visibility to false
+        else
+            viewHolder.otherUsersTextView.setVisibility(View.GONE);
 
         return convertView;
     }
@@ -84,6 +108,7 @@ public class TrackAdapter extends BaseAdapter {
     private class TrackViewHolder {
         TextView title;
         TextView artist;
-        Button userInitial;
+        TextView initialTextView;
+        TextView otherUsersTextView;
     }
 }
